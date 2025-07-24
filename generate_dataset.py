9966b6c33034e899b6b3c9888848b1186d579a09
Capsaicinno <isshuf@gmail.com>
2025-07-24 23:47:05 +0900

    コードブックの一つずつを生成

--- generate_dataset.py
+++ generate_dataset.py
@@ -5,6 +5,7 @@ from torchvision.transforms.functional import to_pil_image
 import tqdm
 import os
 from utils.util import to_code,code_to_img
+from PIL import Image
 device = "cpu"
 
 vqvaes = torch.load("vqvae_models.pth")
@@ -23,8 +24,6 @@ torch.backends.cudnn.benchmark=True
 with torch.inference_mode():
     for i,d in enumerate(tqdm.tqdm(dataset)):
         data,path,roll_pixel = d
-        patch_size = 128
-        data[:,:patch_size, :patch_size] = 1
         #data = img = torch.from_numpy(cv2.resize(img,(512,256))).permute(2,0,1)
         data = to_code(data,encoder,quantizer,device)
         np.savez_compressed(f"datas_all_one/{i:06}.npz",data=data,path=np.array([path]),degree=np.array([roll_pixel]))
@@ -32,6 +31,7 @@ with torch.inference_mode():
             img = to_pil_image(code_to_img(data,quantizer,decoder,device)[0])
             img.save(f"vqvae_recon_one/{os.path.basename(path)}")
 
+#original
 with torch.inference_mode():
     for i,d in enumerate(tqdm.tqdm(dataset_test)):
         data,path,roll_pixel = d
@@ -41,3 +41,53 @@ with torch.inference_mode():
         if roll_pixel==0:
             img = to_pil_image(code_to_img(data,quantizer,decoder,device)[0])
             img.save(f"vqvae_recon_one_test/{os.path.basename(path)}")
+
+###############PATTERN_1   test用データ　エンコード前に画像の左上を白に加工##########################
+# with torch.inference_mode():
+#     for i,d in enumerate(tqdm.tqdm(dataset_test)):
+#         data,path,roll_pixel = d
+#         patch_size = 128
+#         data[:,:patch_size, :patch_size] = 1
+#         #data = img = torch.from_numpy(cv2.resize(img,(512,256))).permute(2,0,1)
+#         data = to_code(data,encoder,quantizer,device)
+#         np.savez_compressed(f"datas_all_test_one/{i:06}.npz",data=data,path=np.array([path]),degree=np.array([roll_pixel]))
+#         if roll_pixel==0:
+#             img = to_pil_image(code_to_img(data,quantizer,decoder,device)[0])
+#             img.save(f"vqvae_recon_one_test/{os.path.basename(path)}")
+
+
+###############PATTERN_2   test用データ　エンコーダ後にコードブックの左上を白に加工#####################
+with torch.inference_mode():
+    for i,d in enumerate(tqdm.tqdm(dataset_test)):
+        data,path,roll_pixel = d
+        #data = img = torch.from_numpy(cv2.resize(img,(512,256))).permute(2,0,1)
+        data = to_code(data,encoder,quantizer,device)
+        
+        #codebookの中身を変更
+        patch_size = 4
+        max_idx = data.max()
+        data = data.reshape(16,32)#flattenされていた部分を長方形型に
+        data[:patch_size,:patch_size] = max_idx
+        data = data.flatten()#flatに戻す
+        
+
+        np.savez_compressed(f"datas_all_test_one/{i:06}.npz",data=data,path=np.array([path]),degree=np.array([roll_pixel]))
+        if roll_pixel==0:
+            img = to_pil_image(code_to_img(data,quantizer,decoder,device)[0])
+            img.save(f"vqvae_recon_one_test/{os.path.basename(path)}")
+
+###############PATTERN_3  コードブックを分けて別々で生成　その後画像を並べて結合　##############################
+with torch.inference_mode():
+    for i,d in enumerate(tqdm.tqdm(dataset_test)):
+        data,path,roll_pixel = d
+        #data = img = torch.from_numpy(cv2.resize(img,(512,256))).permute(2,0,1)
+        data = to_code(data,encoder,quantizer,device)
+        np.savez_compressed(f"datas_all_test_one/{i:06}.npz",data=data,path=np.array([path]),degree=np.array([roll_pixel]))
+        if roll_pixel==0:
+            concat_img = Image.new('RGB',(512,256))
+            for i in range(16):
+                for j in range(32):
+                    code = data[i+j]
+                    img_patch = to_pil_image(code_to_img(data,quantizer,decoder,device)[0])
+                    concat_img.paste(img_patch,(16*j,16*i))
+            concat_img.save(f"vqvae_recon_one_test/{os.path.basename(path)}")
